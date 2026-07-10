519a82ce284c3684f1dd90dad71e05145a69f67c
kawiggles <kawikadembroski@gmail.com>
2026-07-09 22:45:32 -0600

    bleh: i don't know how tokio works

--- server/src/tcp/request.rs
+++ server/src/tcp/request.rs
@@ -2,7 +2,7 @@ use crate::logs::{TcpErr, DbErr};
 use crate::query::Query;
 use log::{info, warn};
 
-use tokio::io::{AsyncRead, AsyncWriteExt};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, Result};
 
 #[derive(Debug)]
 pub struct StartupMessage(usize); // where value is version
@@ -13,24 +13,26 @@ pub enum Request {
     Termination
 }
 
-pub async fn decode_startup<T: AsyncRead + AsyncWriteExt>(stream: &mut T) -> Result<StartupMessage, TcpErr> {
+pub async fn decode_startup<T: AsyncReadExt + Unpin + AsyncWriteExt>
+    (stream: &mut T) -> Result<StartupMessage> {
+
     info!(" - Decoding client startup message");
-    let len = read_i32(stream)?;
-    let code = read_i32(stream)?;
+    let len = read_i32(stream);
+    let code = read_i32(stream);
     info!("   - Code is {}", code);
 
     match code {
         // SSL negotiation, WAYYYY down the line
         80877103 => {
             info!("   - Negotiating SSL with client");
-            stream.write_all(&[b'N'])?;
-            decode_startup(stream)
+            stream.write_all(&[b'N']);
+            decode_startup(stream).await;
         },
         196608 => {
             info!("   - Postgresql client is using protocol version 3.0");
             // TODO: Parse user parameter
             let mut params = vec![0u8; (len - 8) as usize];
-            stream.read_exact(&mut params)?;
+            stream.read_exact(&mut params);
 
             Ok(StartupMessage(code as usize))
         },
@@ -41,7 +43,7 @@ pub async fn decode_startup<T: AsyncRead + AsyncWriteExt>(stream: &mut T) -> Res
     }
 }
 
-pub async fn decode_request<T: AsyncRead>(stream: &mut T) -> Result<Request, DbErr> {
+pub async fn decode_request<T: AsyncReadExt + Unpin>(stream: &mut T) -> Result<Request, DbErr> {
     info!(" - Decoding client request");
     let message_type = read_char(stream)?;
     info!("   - Message type is {}", message_type);
@@ -61,36 +63,31 @@ pub async fn decode_request<T: AsyncRead>(stream: &mut T) -> Result<Request, DbE
     }
 }
 
-fn _read_i16<T: Read>(stream: &mut T) -> Result<i16, TcpErr> {
+fn _read_i16<T: AsyncReadExt + Unpin>(stream: &mut T) -> i16 {
     let mut buf = [0u8; 2];
-    stream.read_exact(&mut buf)?;
-    Ok(i16::from_be_bytes(buf))
+    stream.read_exact(&mut buf);
+    i16::from_be_bytes(buf)
 }
 
-fn read_i32<T: Read>(stream: &mut T) -> Result<i32, TcpErr> {
+fn read_i32<T: AsyncReadExt + Unpin>(stream: &mut T) -> i32 {
     let mut buf = [0u8; 4];
-    stream.read_exact(&mut buf)?;
-    Ok(i32::from_be_bytes(buf))
+    stream.read_exact(&mut buf);
+    i32::from_be_bytes(buf)
 }
 
 // TODO: validate that len is correct
 // The len being passed is the len value from the message
-fn read_contents<T: Read>(stream: &mut T, len: i32) -> Result<String, TcpErr> {
+fn read_contents<T: AsyncReadExt + Unpin>(stream: &mut T, len: i32) -> Result<String, TcpErr> {
     let mut buf = vec![0; (len - 4) as usize];
-    stream.read_exact(&mut buf)?;
+    stream.read_exact(&mut buf);
     let contents = String::from_utf8(buf)?
         .trim_matches('\0')
         .to_string();
     Ok(contents)
 }
 
-fn read_char<T: Read>(stream: &mut T) -> Result<char, TcpErr> {
+fn read_char<T: AsyncReadExt + Unpin>(stream: &mut T) -> char {
     let mut buf = [0u8; 1];
-    match stream.read_exact(&mut buf) {
-        Ok(_) => Ok(buf[0] as char),
-        Err(e) if e.kind() == std::io::ErrorKind::UnexpectedEof => {
-            Err(TcpErr::ClientDisconnected)
-        },
-        Err(e) => Err(TcpErr::IOErr(e)),
-    }
+    stream.read_exact(&mut buf);
+    buf[0] as char
 }
